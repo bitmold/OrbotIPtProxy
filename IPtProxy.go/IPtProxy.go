40fe03b3fb868abf855a640583e38626eb16b75e
n8fr8 <nathan@freitas.net>
2022-01-31 14:22:05 -0500

    add tun2socks support

--- IPtProxy.go/IPtProxy.go
+++ IPtProxy.go/IPtProxy.go
@@ -12,6 +12,8 @@ import (
 	"runtime"
 	"strconv"
 	"time"
+	"github.com/eycorsican/go-tun2socks/core"
+	"github.com/eycorsican/go-tun2socks/proxy/socks"
 )
 
 var meekPort = 47000
@@ -350,3 +352,33 @@ func isAvailable(port int) bool {
 
 	return false
 }
+
+
+var lwipStack core.LWIPStack
+
+// PacketFlow should be implemented in Java/Kotlin.
+type PacketFlow interface {
+	// WritePacket should writes packets to the TUN fd.
+	WritePacket(packet []byte)
+}
+
+// Write IP packets to the lwIP stack. Call this function in the main loop of
+// the VpnService in Java/Kotlin, which should reads packets from the TUN fd.
+func InputPacket(data []byte) {
+	if lwipStack != nil {
+		lwipStack.Write(data)
+	}
+}
+
+func StartSocks(packetFlow PacketFlow, proxyHost string, proxyPort int) {
+	if packetFlow != nil {
+		lwipStack = core.NewLWIPStack()
+		core.RegisterTCPConnHandler(socks.NewTCPHandler(proxyHost, uint16(proxyPort)))
+		core.RegisterUDPConnHandler(socks.NewUDPHandler(proxyHost, uint16(proxyPort), 2*time.Minute))
+		core.RegisterOutputFn(func(data []byte) (int, error) {
+			packetFlow.WritePacket(data)
+			return len(data), nil
+		})
+	}
+}
+
