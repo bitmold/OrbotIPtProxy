94c7231237b62f9d54018c4d5dc665eec0b3074c
a <dsnake@protonmail.com>
2024-01-23 18:00:43 -0500

    Build OrbotLib in tempdir like IPtProxy...

--- build-orbot.sh
+++ build-orbot.sh
@@ -24,6 +24,7 @@ else
   cd IPtProxy || exit 1
 fi
 
+
 printf '\n\n--- Apply patches to lyrebird and snowflake...\n'
 patch --directory=lyrebird --strip=1 < lyrebird.patch
 patch --directory=snowflake --strip=1 < snowflake.patch
@@ -32,9 +33,18 @@ cd ..
 cp OrbotTun.go/* IPtProxy/IPtProxy.go/
 
 
-cd IPtProxy/IPtProxy.go
+TEMPDIR="$TMPDIR/IPtProxy"
+printf '\n\n--- Prepare build environment at %s...\n' "$TEMPDIR"
+cd IPtProxy
+CURRENT=$PWD
+rm -rf "$TEMPDIR"
+mkdir -p "$TEMPDIR"
+cp -a . "$TEMPDIR/"
+
 printf '\n\n--- Compile %s...\n' "$OUTPUT"
 export PATH=~/go/bin:$PATH
+cd "$TEMPDIR/IPtProxy.go" || exit 1
 gomobile init 
-gomobile bind -o "../../../OrbotLib.aar" -target=android -androidapi=19 -v -trimpath
+gomobile bind -o "OrbotLib.aar" -target=android -androidapi=19 -v -trimpath
+cp -v OrbotLib* "$CURRENT/../.."
 printf '\n\nDone\n'
