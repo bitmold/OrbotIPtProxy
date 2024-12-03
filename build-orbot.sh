3cc3ab292ee526bbeeb422a85b37707ff1539635
n8fr8 <nathan@freitas.net>
2024-12-03 17:17:55 -0500

    update build script for IPtProxy 4.0.0

--- build-orbot.sh
+++ build-orbot.sh
@@ -10,30 +10,12 @@ if [ -d IPtProxy ]; then
   cd IPtProxy
   git clean -fdx
   git reset --hard
-  if [ -d lyrebird ]; then
-    cd lyrebird
-    git clean -fdx
-    git reset --hard
-    cd ..
-  fi
-  if [ -d snowflake ]; then
-    cd snowflake
-    git clean -fdx
-    git reset --hard
-    cd ..
-  fi
-  cd ..
   git submodule update --init --recursive
-  cd IPtProxy
 else
   git submodule update --init --recursive
   cd IPtProxy || exit 1
 fi
 
-
-printf '\n\n--- Apply patches to lyrebird and snowflake...\n'
-patch --directory=lyrebird --strip=1 < lyrebird.patch
-patch --directory=snowflake --strip=1 < snowflake.patch
 
 cd ..
 cp OrbotTun.go/* IPtProxy/IPtProxy.go/
@@ -63,6 +45,6 @@ printf '\n\n--- Compile %s...\n' "$OUTPUT"
 export PATH=~/go/bin:$PATH
 cd "$TEMPDIR/IPtProxy.go" || exit 1
 gomobile init 
-gomobile bind -o "OrbotLib.aar" -ldflags="-w -s" -target=android -androidapi="$MIN_ANDROID_SDK" -v -trimpath
-cp -v OrbotLib* "$CURRENT/../.."
+gomobile bind -o "OrbotLib.aar" -ldflags="-w -s -checklinkname=0" -target=android -androidapi="$MIN_ANDROID_SDK" -v -trimpath
+cp -v OrbotLib* "$CURRENT/.."
 printf '\n\nDone\n'
